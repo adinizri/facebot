a9210190e6c3feba21ed942ca31c9f3d4120adf0
Train10 <Train10@eladsystems.co.il>
2019-02-03 11:43:41 +0200

    new code

--- app.js
+++ app.js
@@ -23,103 +23,6 @@ app.get("/webhook", function (req, res) {
     res.sendStatus(403);
   }
 });
-// All callbacks for Messenger will be POST-ed here
-app.post("/webhook", function (req, res) {
-    // Make sure this is a page subscription
-    if (req.body.object == "page") {
-      // Iterate over each entry
-      // There may be multiple entries if batched
-      req.body.entry.forEach(function(entry) {
-        // Iterate over each messaging event
-        entry.messaging.forEach(function(event) {
-          if (event.postback) {
-            processPostback(event);
-          }
-        });
-      });
-  
-      res.sendStatus(200);
-    }
-  });
-  
-  function processPostback(event) {
-    var senderId = event.sender.id;
-    var payload = event.postback.payload;
-  
-    if (payload === "Greeting") {
-      // Get user's first name from the User Profile API
-      // and include it in the greeting
-      request({
-        url: "https://graph.facebook.com/v2.6/" + senderId,
-        qs: {
-          access_token: process.env.PAGE_ACCESS_TOKEN,
-          fields: "first_name"
-        },
-        method: "GET"
-      }, function(error, response, body) {
-        var greeting = "";
-        if (error) {
-          console.log("Error getting user's name: " +  error);
-        } else {
-          var bodyObj = JSON.parse(body);
-          name = bodyObj.first_name;
-          greeting = "Hi " + name + ". ";
-        }
-        var message = greeting +"I'm SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-        sendMessage(senderId, {text: message});
-      });
-    }
-  }
-  
-  // sends message to user
-  function sendMessage(recipientId, message) {
-    request({
-      url: "https://graph.facebook.com/v2.6/me/messages",
-      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-      method: "POST",
-      json: {
-        recipient: {id: recipientId},
-        message: message,
-      }
-    }, function(error, response, body) {
-      if (error) {
-        console.log("Error sending message: " + response.error);
-      }
-    });
-
-
-
-
-
-    var express = require("express");
-var request = require("request");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-
-var db = mongoose.connect(process.env.MONGODB_URI);
-var Movie = require("./models/movie");
-
-var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 5000));
-
-// Server index page
-app.get("/", function (req, res) {
-    res.send("Deployed!");
-});
-
-// Facebook Webhook
-// Used for verification
-app.get("/webhook", function (req, res) {
-    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
-        console.log("Verified webhook");
-        res.status(200).send(req.query["hub.challenge"]);
-    } else {
-        console.error("Verification failed. The tokens do not match.");
-        res.sendStatus(403);
-    }
-});
 
 // All callbacks for Messenger will be POST-ed here
 app.post("/webhook", function (req, res) {
@@ -210,8 +113,8 @@ function processMessage(event) {
 }
 
 function findMovie(userId, movieTitle) {
-    request("http://www.omdbapi.com/?type=movie&t=" + movieTitle, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
+    request(`http://www.omdbapi.com/?apiKey=${apiKey}&type=movie&t=${movieTitle}`, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
             var movieObj = JSON.parse(body);
             if (movieObj.Response === "True") {
                 var query = {user_id: userId};
@@ -292,4 +195,3 @@ function sendMessage(recipientId, message) {
         }
     });
 }
-  }
